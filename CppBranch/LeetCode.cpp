d0e1cfd2c5293954227547516a29d44fcc6d44b8
wh1tedrag0n <1604973242@qq.com>
2024-12-29 14:20:27 +0800

    24.12.29

--- CppBranch/LeetCode.cpp
+++ CppBranch/LeetCode.cpp
@@ -1,20 +1,72 @@
-﻿// LeetCode.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Solution721 sol;
+	vector<vector<string>> temp{ {"John", "johnsmith@mail.com", "john_newyork@mail.com"},
+		{"John", "johnsmith@mail.com", "john00@mail.com"},{"Mary", "mary@mail.com"},
+		{"John", "johnnybravo@mail.com"} };
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+class Solution721 {
+private:
+	vector<vector<string>> Union;
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+	int Find(vector<string> input) {
+		int index = 0;
+		for (const auto& item : Union)
+		{
+			for (int i = 1; i < item.size(); i++)
+			{
+				for (int j = 1; j < input.size(); j++)
+				{
+					if (item[i] == input[j])
+					{
+						return index;
+					}
+				}
+			}
+			index++;
+		}
+		return -1;
+	}
+
+	void Merge(int index, vector<string> account) {
+		for (int i = 1; i < account.size(); i++) {
+			for (int j = 1; j < Union[index].size(); j++) {
+				if (account[i] == Union[index][j]) {
+					break;
+				}
+			}
+
+			Union[index].push_back(account[i]);
+			sort(Union[index].begin() + 1, Union[index].end());
+		}
+	}
+
+public:
+	vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+		for (const auto& account : accounts)
+		{
+			int index = Find(account);
+			if (index != -1)
+			{
+				Merge(index, account);
+			}
+			else
+			{
+				Union.push_back(account);
+			}
+		}
+		return Union;
+	}
+};
+
+class Solution {
+public:
+	std::vector<int> findRedundantConnection(std::vector<std::vector<int>>& edges) {
+	}
+};
